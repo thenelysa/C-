de4817ac1757a3a712759fc4e3ffbfd17fb194a7
thenelysa <neelisha.shrsth@gmail.com>
2022-12-07 09:17:18 +0545

    Update

--- Docupload/Docupload/Form1.Designer.cs
+++ Docupload/Docupload/Form1.Designer.cs
@@ -28,12 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(47, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "file path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(47, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 30);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "file directory";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(47, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 44);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Browse File ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(47, 194);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(179, 48);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Upload Document";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private Button button2;
+        private OpenFileDialog openFileDialog1;
     }
 }
--- Docupload/Docupload/Form1.cs
+++ Docupload/Docupload/Form1.cs
@@ -1,3 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+
 namespace Docupload
 {
     public partial class Form1 : Form
@@ -5,6 +17,72 @@ namespace Docupload
         public Form1()
         {
             InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection(@"Data Source = .\SQLEXPRESS;
+                                                Initial Catalog=datafile;
+                                                Integrated Security=True");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //To where your opendialog box get starting location. My initial directory location is desktop.
+            openFileDialog1.InitialDirectory = "C://Desktop";
+            //Your opendialog box title name.
+            openFileDialog1.Title = "Select file to be upload.";
+            //which type file format you want to upload in database. just add them.
+            openFileDialog1.Filter = "Select Valid Document(*.pdf; *.doc; *.xlsx; *.html)|*.pdf; *.docx; *.xlsx; *.html";
+            //FilterIndex property represents the index of the filter currently selected in the file dialog box.
+            openFileDialog1.FilterIndex = 1;
+            try
+            {
+                if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    if (openFileDialog1.CheckFileExists)
+                    {
+                        string path = System.IO.Path.GetFullPath(openFileDialog1.FileName);
+                        label1.Text = path;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please Upload document.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string filename = System.IO.Path.GetFileName(openFileDialog1.FileName);
+                if (filename == null)
+                {
+                    MessageBox.Show("Please select a valid document.");
+                }
+                else
+                {
+                    //we already define our connection globaly. We are just calling the object of connection.
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("insert into doc (document)values('\\Document\\" + filename + "')", con);
+                    string path = Application.StartupPath.Substring(0, (Application.StartupPath.Length - 10));
+                    System.IO.File.Copy(openFileDialog1.FileName, path + "\\Document\\" + filename);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    MessageBox.Show("Document uploaded.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
